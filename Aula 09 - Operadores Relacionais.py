cda471e116d65f7ecc8dcf65ff979355ca821d55
Leonardo mota <lamancio81@gmail.com>
2024-12-09 15:53:06 -0300

    aula 09

---
+++ Aula 09 - Operadores Relacionais.py
@@ -0,0 +1,23 @@
+# != diferentes 
+
+A = 10
+B = 15
+C = 25
+
+print('valor de A maior que B : ', A > B)
+print('valor de A menor que B : ', A < B)
+print('valor de A diferente do  que B : ', A != B)
+
+# IF 
+
+if B > A:
+    print('B é maior que A')
+else:
+    print('A maior que B')
+
+# While
+
+while B >= A:
+    print('A agora vale', A)
+    A += 1
+
