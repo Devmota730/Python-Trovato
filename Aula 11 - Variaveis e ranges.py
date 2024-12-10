ec74f4cbe72ba98c4cb1a753da32043cda9dbca7
Leonardo mota <lamancio81@gmail.com>
2024-12-10 13:32:04 -0300

    aula 11 range

---
+++ Aula 11 - Variaveis e ranges.py
@@ -0,0 +1,25 @@
+# escopor das variaveis
+
+def Funcao1(a):
+    print(a + varF)
+def Funcao2(a):
+    varG = 100
+    print(a * varF + 100)
+
+varF = int(input('Digiti um numero: '))
+
+Funcao1(int(input('qual o numero da função 1:')))
+Funcao2(int(input('qual o numero da funcao 2:')))
+print()
+
+varH = range(4)
+print(varH)
+
+print(list(varH)) # mostra a faixa da range
+
+print(list(range(1, 10,2 ))) # o terceiro valor é o incrementador
+
+
+
+
+
