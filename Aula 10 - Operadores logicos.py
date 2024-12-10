01f8d46a0bfd6640c0b7d5330b2f51b6202edcdf
Leonardo mota <lamancio81@gmail.com>
2024-12-10 09:53:29 -0300

    Aula 10

---
+++ Aula 10 - Operadores logicos.py
@@ -0,0 +1,23 @@
+# operadores de associação
+
+x = range(1, 10)
+
+print('1 e 12 na faixa', 1 and 13 in x)
+print('1 no in x: ', 1 not in x) # se não tiver é true, else traz false
+print('15 and 8 not i x:', 15 and 8 not in x)
+print('8 and 15 not i x:', 8 and 15 not in x)  # true false, traz false
+
+print()
+# operadores logicos
+
+num1 = 9
+num2 = 5
+num = 3
+
+print('num1 é > que num2:', num1 > num2)
+print()
+# not
+
+print(not(False))
+print(not(True))
+print(not(True == False))
